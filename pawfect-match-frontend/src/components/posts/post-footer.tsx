43e19a4904524ac9c92608d361cce6d46f331499
Matan Leibovich <matan6142@gmail.com>
2025-03-01 15:35:22 +0200

    post footer and paw button

--- pawfect-match-frontend/src/components/posts/post-footer.tsx
+++ pawfect-match-frontend/src/components/posts/post-footer.tsx
@@ -1,15 +1,35 @@
 import { Flex } from "@mantine/core";
 import { ThemeIcon } from "@mantine/core";
+import { useEffect, useState } from "react";
 import { IconPawFilled } from "@tabler/icons-react";
 
-const PostFooter: React.FC = () => {
+import { Post } from "../../types/post";
+import { pawGreen } from "../../consts";
+
+const PostFooter: React.FC<Pick<Post, "userId" | "likedBy">> = ({
+  userId,
+  likedBy,
+}) => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const handlePawClick = () => {
+    setIsLiked((isLiked) => !isLiked);
+  };
+
+  useEffect(() => {
+    const isLikedByUser = likedBy.includes(userId);
+    setIsLiked(isLikedByUser);
+  }, [userId, likedBy]);
+
   return (
     <Flex>
       <ThemeIcon variant="white" size={64} color={"dark"}>
         <IconPawFilled
-          style={{ height: "70%", width: "70%" }}
           stroke={1.5}
           cursor={"pointer"}
+          onClick={handlePawClick}
+          color={isLiked ? pawGreen : "dark"}
+          style={{ height: "70%", width: "70%" }}
         />
       </ThemeIcon>
     </Flex>
--- pawfect-match-frontend/src/components/posts/posts-list.tsx
+++ pawfect-match-frontend/src/components/posts/posts-list.tsx
@@ -1,11 +1,16 @@
 import { Stack } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 import { Post } from "./post";
 import { Post as PostType } from "../../types/post";
 import { fetchPosts } from "../../services/post-service";
 
 const PostsList: React.FC = () => {
-  const posts: PostType[] = fetchPosts();
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    setPosts(fetchPosts());
+  }, []);
 
   return (
     <Stack justify={"center"} align={"center"}>
@@ -16,6 +21,7 @@ const PostsList: React.FC = () => {
           content={post.content}
           breed={post.breed}
           imageUrl={post.imageUrl}
+          likedBy={post.likedBy}
           lastUpdated={post.lastUpdated}
         />
       ))}
